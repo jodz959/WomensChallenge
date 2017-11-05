832ac93cb138a3c241394aa01dbffef9da57e855
hamstlanda <hamstlanda@gmail.com>
2017-11-05 05:56:47 -0500

    made progress on the opp and guides page

--- womens_challenge/home/views.py
+++ womens_challenge/home/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+def opportunities(request):
+    return render(request, 'home/opportunities.html')
