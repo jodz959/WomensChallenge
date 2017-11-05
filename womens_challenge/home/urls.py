1011a2f95cd6ac37b3246d5e2f9975198bebae27
Melida Grullon <karinag_07@hotmail.com>
2017-11-05 10:37:11 -0500

    html file headings

--- womens_challenge/home/urls.py
+++ womens_challenge/home/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
 
     url(r'^groups/', views.groups, name='group'),
 
-    url(r'^opportunities/$', views.opportunities, name='opportunities')
+    url(r'^opportunities/$', views.opportunities, name='opportunities'),
+     url(r'^mentor/$', views.opportunities, name='mentor'),
+     url(r'^user/$', views.opportunities, name='user')
+    
+    
 
 ]
