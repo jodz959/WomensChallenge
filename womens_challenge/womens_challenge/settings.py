f88e37d2b46fbb152b706384a8a8315683c02ec9
Jody <smile.jody@gmail.com>
2017-11-04 23:38:05 -0400

    Added carousel and images to index

--- womens_challenge/womens_challenge/settings.py
+++ womens_challenge/womens_challenge/settings.py
@@ -114,8 +114,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL  = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
